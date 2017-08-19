a3af21eb4264bdd8cc0bdd357e7859fa745648ee
Bo Shen <99686@qq.com>
2017-08-19 15:41:26 +0800

    Added data model:form

--- pages/submit/submit.js
+++ pages/submit/submit.js
@@ -1,4 +1,8 @@
 // pages/submit/submit.js
+
+const AV = require('../../libs/av-weapp-min.js');
+const form = require('../../model/form.js');
+
 Page({
 
   /**
@@ -12,7 +16,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
